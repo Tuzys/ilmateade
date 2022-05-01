baa647c879572b4b366c44ee335a0199907b5990
Laura Tõnisson <Laura.Tonisson@tptlive.ee>
2022-05-01 11:46:06 +0300

    sunshine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const container = document.querySelector('.container');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const renderWeather = async() => {
+    let uri = 'https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=46.8&lon=8.2'
+
+const res = await fetch(uri);
+const weathers = await res.json();
+
+weather = weathers.properties.timeseries
+
+let template = '';
+weather.forEach(post => {
+    template += `
+    <div class="post">
+        <h3>${post.time.slice(11,16)}</h3>
+        <h4>${post.time.slice(0,10)}</h4>
+        <p>Temperatuur: ${post.data.instant.details.air_temperature} Celsius</p>
+        <p>Niiskus: ${post.data.instant.details.relative_humidity}%</p>
+        <p>Tuule suund: ${post.data.instant.details.wind_from_direction}</p>
+        <p>Tuule kiirus: ${post.data.instant.details.wind_speed}</p>
+        <p>Pilveala osa: ${post.data.instant.details.cloud_area_fraction}</p>
+        <p>õhurõhk meretasemel: ${post.data.instant.details.air_pressure_at_sea_level}</p>
+    </div> 
+    `
+});
+    container.innerHTML = template;
+};
+
+window.addEventListener('DOMContentLoaded', () => renderWeather());
